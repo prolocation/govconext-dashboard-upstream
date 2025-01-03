4c8a021cbb97fe1199851009d62ac363e4790d43
Prolo <unix@prolocation.net>
2025-01-03 11:14:51 +0100

    Actually push to repo

--- dashboard-server/pushtodocker.sh
+++ dashboard-server/pushtodocker.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 IMAGENAME="ghcr.io/openconext/openconext-dashboard/dashboard-server"
+VERSION=13.0.0
 
 mvn clean install -DskipTests
-docker build --tag ${IMAGENAME} -f docker/Dockerfile .
+docker build --tag ${IMAGENAME}:${VERSION} -f docker/Dockerfile .
+
+# Push to registry
+docker tag ${IMAGENAME}:${VERSION} $(hostname -f)/${IMAGENAME}:${VERSION}
+docker push $(hostname -f)/${IMAGENAME}:${VERSION}
