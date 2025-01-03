008bfaa7f022240bfd0764ba1ac0876b7c56ff0b
Prolo <unix@prolocation.net>
2025-01-03 11:06:00 +0100

    Fix url

--- dashboard-server/pushtodocker.sh
+++ dashboard-server/pushtodocker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGENAME="ghcr.io/openconext/openconext-invite/dashboard-server"
+IMAGENAME="ghcr.io/openconext/openconext-dashboard/dashboard-server"
 
 mvn clean install -DskipTests
 docker build --tag ${IMAGENAME} -f docker/Dockerfile .
