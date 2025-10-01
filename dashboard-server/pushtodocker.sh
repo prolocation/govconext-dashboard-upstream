86c8d2c3b32f119247223814c86f24c537edb06d
Emiel Kollof <emiel@prolocation.net>
2025-10-01 11:08:03 +0200

    additional files

---
+++ dashboard-server/pushtodocker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+IMAGENAME="ghcr.io/openconext/openconext-dashboard/dashboard-server"
+#VERSION=12.3.18.1
+VERSION=13.0.6
+
+mvn clean install -DskipTests
+docker build --tag ${IMAGENAME}:${VERSION} -f docker/Dockerfile .
+
+# Push to registry
+docker tag ${IMAGENAME}:${VERSION} $(hostname -f)/${IMAGENAME}:${VERSION}
+docker push $(hostname -f)/${IMAGENAME}:${VERSION}
